82c08696d15785128f122589a3a9bfd9665e972b
sanjana112000 <sanjanaraj112000@gmail.com>
2023-01-30 15:35:07 +0530

    first commit

---
+++ src/components/Search.js
@@ -0,0 +1,26 @@
+import React from 'react';
+//import image20 from '../images/search.png';
+
+
+
+function Search() {
+    return (
+       <div className='searchs'>
+
+        <div className='search s1'>
+        
+        <input id='s22'
+
+     key="search-bar"
+
+     placeholder="Search a Term | Topic"
+    
+    />
+</div>
+
+      </div>
+  
+    );
+}
+
+export default Search;
